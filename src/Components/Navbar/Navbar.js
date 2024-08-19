2511a568709bf012e6e56d418090fa19a4a5ce96
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-08-19 14:06:13 +0530

    floder created

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css"
+const Navbar=()=>{
+    return(
+        <>
+            <ul className="NavL">
+                <a href="/">Home</a>
+                <a href="/About">About</a>
+            </ul>
+        </>
+    )
+}
+export default Navbar;
